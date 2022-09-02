c566ece7dd097c57b6f044a21813eb7b460a7b44
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-03 00:53:43 +0500

    Added landing-page component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import LandingPage from "../Components/Landing-page/Landing-page";
 
 export default function Home() {
-  return <div className="App">hello world</div>;
+  return <LandingPage />;
 }
