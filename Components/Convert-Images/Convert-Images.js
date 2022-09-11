e276a4c7bd94c76435db9f229495c04d3e2ee284
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-11 21:11:14 +0500

    Some responsive fixes

--- Components/Convert-Images/Convert-Images.js
+++ Components/Convert-Images/Convert-Images.js
@@ -67,7 +67,7 @@ export default function ConvertImages() {
               />
             </div>
           </Col>
-          <Col lg={10} md={10} sm={10} className="my-2">
+          <Col lg={8} md={10} sm={10} className="my-2">
             <div className="d-flex flex-row justify-content-between align-items-center">
               <p className="me-4 mb-0 text-left fs-6">
                 <b>Note:</b> We only store you image for the life span of
