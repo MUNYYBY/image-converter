fa6e6593507778567d47dcecfd0755f8efaf48f2
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-18 17:45:14 +0500

    Initiated upload-images api

--- pages/api/upload-images.js
+++ pages/api/upload-images.js
@@ -1,3 +1,19 @@
+import fs from "fs";
+import formidable from "formidable";
+import path from "path";
+import getConfig from "next/config";
+const sharp = require("sharp");
+
+const serverPath = (staticFilePath) => {
+  return path.join(
+    getConfig().serverRuntimeConfig.PROJECT_ROOT,
+    staticFilePath
+  );
+};
+
 export default async function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(405).json({ error: `Method '${req.method}' not allowed! ❌` });
+  }
   res.status(200).json("Upload images end point");
 }
