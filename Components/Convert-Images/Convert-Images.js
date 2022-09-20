fef3dabf27a67f407d292ba05421b3e4aa106255
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-21 01:57:24 +0500

    Only for commits

--- Components/Convert-Images/Convert-Images.js
+++ Components/Convert-Images/Convert-Images.js
@@ -254,7 +254,7 @@ export default function ConvertImages() {
                   irure dolor in reprehenderit in voluptate velit esse cillum
                   dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                   cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  mollit anim.
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
