5c5df79a129d19d4983ea3b2aed7d9b7a1c1611a
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-23 23:14:40 +0500

    Change user images upload directory in API

--- pages/api/upload-images.js
+++ pages/api/upload-images.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     let newFileName = null;
 
     //Relative directories
-    const rootDir = "/public/assets";
+    const rootDir = "/public/assets/uploads";
 
     // directory for saving images
     let dir = serverPath(rootDir);
