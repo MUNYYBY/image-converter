a19da9b182838d0f8e9312fb8a5d30aa28b4dba6
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-24 01:16:45 +0500

    Removed tempValue console leak

--- Components/Convert-Images/Convert-Images.js
+++ Components/Convert-Images/Convert-Images.js
@@ -89,7 +89,6 @@ export default function ConvertImages() {
             uploadProgress: uploadProgressCompleted,
           };
           setImagesToBeUploaded(tempValue);
-          console.log("Hello", tempValue);
         },
       });
     }
