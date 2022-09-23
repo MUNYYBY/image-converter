b55edfe3251a305cb48258db5a6473d1f79feee4
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-24 01:41:26 +0500

    Initiated backgroud loading animation

--- Components/Image-Container/Image-Container.js
+++ Components/Image-Container/Image-Container.js
@@ -6,36 +6,52 @@ export default function ImageContainer(props) {
   //     console.log("PROPS: ", props.uploadProgress);
   //   }, [props.uploadProgress]);
   return (
-    <div
-      id={styles.ImageContainer}
-      className="bg-body w-100 p-3 my-2 rounded shadow"
-    >
-      <div className="d-flex flex-row justify-content-between align-items-center">
-        <div className="d-flex flex-row justify-content-center align-items-center">
-          <AiOutlineFileImage size={32} />
-          <p className="mb-0 px-2 text-left">{props.imageName}</p>
-        </div>
-        <div className="d-flex flex-row justify-content-center align-items-center">
-          <div
-            className="px-2 py-1 rounded "
-            style={{ backgroundColor: "#EBEBEB" }}
-          >
-            <p className="m-0">{props.imageSize}</p>
+    <div id={styles.ImageContainer} className="bg-body w-100 rounded shadow">
+      <div className="p-3 my-2">
+        <div className="d-flex flex-row justify-content-between align-items-center">
+          <div className="d-flex flex-row justify-content-center align-items-center">
+            <AiOutlineFileImage size={32} />
+            <p className="mb-0 px-2 text-left">{props.imageName}</p>
           </div>
-          <div
-            className="p-1 ms-4 rounded justify-content-center align-items-center"
-            style={{ backgroundColor: "#D9D9D9" }}
-          >
-            <button
-              type="button"
-              className="btn-close mb-0"
-              aria-label="Close"
-              onClick={() => props.removeImage(props.imageUrl)}
-            ></button>
+          <div className="d-flex flex-row justify-content-center align-items-center">
+            <div
+              className="px-2 py-1 rounded "
+              style={{ backgroundColor: "#EBEBEB" }}
+            >
+              <p className="m-0">{props.imageSize}</p>
+            </div>
+            <div
+              className="p-1 ms-4 rounded justify-content-center align-items-center"
+              style={{ backgroundColor: "#D9D9D9" }}
+            >
+              <button
+                type="button"
+                className="btn-close mb-0"
+                aria-label="Close"
+                onClick={() => props.removeImage(props.imageUrl)}
+              ></button>
+            </div>
           </div>
         </div>
       </div>
-      {props.uploadProgress ? <div className="w-100"></div> : ""}
+      {props.uploadProgress ? (
+        <div className="w-100" style={{ position: "relative" }}>
+          <div
+            style={{
+              position: "absolute",
+              top: -50,
+              left: 0,
+              height: "100%",
+              width: "",
+              backgroundColor: "rgba(0,255,36,0.5)",
+            }}
+          >
+            hello
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
