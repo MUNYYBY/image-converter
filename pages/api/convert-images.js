d2ceb0701698230dc20d2f58d67ed4c486e8ba10
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-09-26 11:50:52 +0500

    Added directory check to the convert-images API

--- pages/api/convert-images.js
+++ pages/api/convert-images.js
@@ -1,8 +1,33 @@
+//Global date holder
+let d = new Date();
+
+const serverPath = (staticFilePath) => {
+  //latest dir for images targeting todays working directory
+  let todaysDir = d.toISOString().split("T")[0];
+  return path.join(
+    String(getConfig().serverRuntimeConfig.PROJECT_ROOT),
+    staticFilePath,
+    todaysDir,
+    "Converted-Images"
+  );
+};
+
 export default async function handler(req, res) {
   if (req.method == "POST") {
     res.status(405).json({ error: `Method '${req.method}' not allowed! ❌` });
   }
+  try {
+    //Relative directories
+    const rootDir = "/public/assets/uploads";
+    // directory for saving images
+    let dir = serverPath(rootDir);
 
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  } catch (error) {
+    console.log("❌ Error while converting images:", error);
+  }
   const { imageUrl, conversionQuality } = req.query;
 
   console.log(imageUrl, conversionQuality);
