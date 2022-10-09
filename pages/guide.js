8dd171317276a8f3c52c5a7870ff29ad5419cf10
HM-MUNEEB <hm.muneeb.u.r@gmail.com>
2022-10-09 22:39:32 +0500

    Workin on guide page 2

--- pages/guide.js
+++ pages/guide.js
@@ -2,16 +2,18 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { AiOutlineFileImage } from "react-icons/ai";
 
 export default function Help() {
   return (
     <main className="Guide-to-all-the-tools">
       <Container>
-        <Row
-          className="d-flex flex-column justify-content-center align-items-center rounded py-5 my-4"
-          style={{ backgroundColor: "rgba(0,0,0,0.08)" }}
-        >
-          <Col sm={10}>
+        <Row className="d-flex flex-column justify-content-center align-items-center my-4">
+          <Col
+            sm={10}
+            className="rounded py-5"
+            style={{ backgroundColor: "rgba(0,0,0,0.08)" }}
+          >
             <div className="d-flex flex-column justify-content-center align-items-center">
               <h1 className="mb-0">Guide</h1>
               <p className="mb-0 text-center">
@@ -19,7 +21,18 @@ export default function Help() {
               </p>
             </div>
           </Col>
-          <Col></Col>
+          <Col lg={4} md={6} sm={10} className="mt-4">
+            <div
+              className="rounded"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.05)",
+                border: "rgba(0, 0, 0, 0.5) 2px solid",
+              }}
+            >
+              <AiOutlineFileImage size={32} />
+              <h4>Image-Converter</h4>
+            </div>
+          </Col>
         </Row>
       </Container>
     </main>
